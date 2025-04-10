28342f669242122e1e1b54f59d233ac99f3347d2
bsoft-albin <albinantony@dbcyelagiri.edu.in>
2025-04-10 10:02:46 +0530

    Project references chnaged.

---
+++ AlbinMicroService.Libraries/Common/UseComponents.cs
@@ -0,0 +1,10 @@
+﻿namespace AlbinMicroService.Libraries.Common
+{
+    public static class UseComponents
+    {
+        public static T GetInstanceOf<T>() where T : new()
+        {
+            return new T();
+        }
+    }
+}
