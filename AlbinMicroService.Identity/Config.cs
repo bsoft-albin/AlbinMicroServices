00bde870874219c38a0ef00931cff74405a8c5e8
bsoft-albin <albinantony@dbcyelagiri.edu.in>
2025-04-22 15:26:20 +0530

    XUnit Test Project only added.

--- AlbinMicroService.Identity/Config.cs
+++ AlbinMicroService.Identity/Config.cs
@@ -6,40 +6,41 @@ namespace AlbinMicroService.Identity
     public class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[] {
+        [
             new IdentityResources.OpenId(),
             new IdentityResources.Profile()
-        };
+        ];
 
         public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[] {
+            [
             new ApiScope("master.read", "Read Access to Master Api")
-            };
+            ];
 
         public static IEnumerable<Client> Clients =>
-            new Client[] {
+            [
             new Client {
                 ClientId = "albin-microservice-client",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = { new Secret("secret".Sha256()) },
-                AllowedScopes = { "master.read" }
+                AllowedScopes = { "master.read" },
+                AccessTokenLifetime = 60
             }
-            };
+            ];
 
         public static List<TestUser> Users =>
-            new List<TestUser> {
+            [
             new TestUser {
                 SubjectId = "1",
                 Username = "testuser",
                 Password = "password"
             }
-            };
+            ];
 
         public static IEnumerable<ApiResource> ApiResources =>
-            new[] {
+            [
             new ApiResource("masterapi", "master Api") {
                 Scopes = { "master.read" }
             }
-            };
+            ];
     }
 }
